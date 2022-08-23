93a2495067ea5ff62863471f3e39af8350484907
DharmikP24 <dharmik.jmp@gmail.com>
2022-08-23 19:43:40 +0530

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    val a: Int = 10
+    val b: Float = 1.5F
+    var c: String = "dharmik"
+    var d: Char = 'N'
+    val e: Boolean = false
+    val f: Double = 7.14
+    val g: Long = 75934035
+    val h: Short = -4
+    val i: Byte = 123
+    println("Integer value: $a")
+    println("Float value: $b")
+    println("String value: $c")
+    println("Character value: $d")
+    println("Boolean value: $e")
+    println("Double value: $f")
+    println("Long value: $g")
+    println("Short value: $h")
+    println("Byte value: $i")
+}
